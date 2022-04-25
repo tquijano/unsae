292ffb9b2c9cd2e00a9a8cb1c53f9058ebc6ec13
judsantamariaro <judsantamariaro@unal.edu.co>
2022-04-25 15:01:56 -0500

    General Info content

--- src/templates/generalInfo/GeneralInfo.jsx
+++ src/templates/generalInfo/GeneralInfo.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import './GeneralInfo.scss'
+import GInfoConceptual from '../../molecules/gInfoConceptual/GInfoConceptual'
+import GInfoGoals from '../../molecules/gInfoGoals/GInfoGoals'
+import GInfoJustification from '../../molecules/gInfoJustification/GInfoJustification'
+import GInfoModels from '../../molecules/gInfoModels/GInfoModels'
 
 const GeneralInfo = () => {
   return (
-    <div>GeneralInfo</div>
+    <div className="generalInfo-container">
+      <GInfoJustification />
+      <GInfoConceptual />
+      <GInfoGoals />
+      <GInfoModels />
+    </div>
   )
 }
 
