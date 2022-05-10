04b6d21118d9e121ee730b36a0cc3a08a1e7846c
Tania Quijano <tquijano@unal.edu.co>
2022-05-09 22:06:25 -0500

    conexion docentes

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,22 +1,22 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { startChecking, startLogin } from '../actions/auth';
+import { startChecking } from '../actions/auth';
 import Bienestar from '../templates/bienestar/Bienestar';
 import GeneralInfo from '../templates/generalInfo/GeneralInfo';
 import Home from '../templates/home/Home';
 import Legal from '../templates/legal/Legal';
-import CoordinatorRoutes from './CoordinatorRoutes';
+// import CoordinatorRoutes from './CoordinatorRoutes';
 import LoginRoutes from './LoginRoutes';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
-import StudentRoutes from './StudentRoutes';
-import TeacherRoutes from './TeacherRoutes';
+// import StudentRoutes from './StudentRoutes';
+// import TeacherRoutes from './TeacherRoutes';
 
 const AppRoutes = () => {
 
   const dispatch = useDispatch();
-  const { checking, typeUser } = useSelector( state => state.auth);
+  const { checking } = useSelector( state => state.auth);
 
   useEffect(() => {
     dispatch ( startChecking() );
@@ -33,13 +33,13 @@ const AppRoutes = () => {
         <Route path="/Informacion-General" element={<GeneralInfo/>}/>
         <Route path="/Informacion-Legal" element={<Legal/>}/>
         <Route path="/Bienestar" element={<Bienestar/>}/>
-        {/* <Route path="/" 
+        <Route path="/" 
           element={
             <PublicRoute>
               <Home/>
             </PublicRoute>
           }
-        /> */}
+        />
 
         {/* <Route path="/Estudiante/*" 
           element={
